0f02bac720a36e65373d753997a16da7bb0f36bb
A330454 <jacobo9cch@gmail.com>
2023-02-07 18:46:50 -0600

    nada que commitear

--- emprendedor_back/settings.py
+++ emprendedor_back/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('llave_secreta')
+SECRET_KEY = "q806FJ3vg3YUjuR8i76EtY2DLWi2GeVfk2CJuaqPTYtOyaqiK9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
