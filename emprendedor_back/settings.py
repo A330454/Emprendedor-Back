5308519f4664e0248ee030094561f940c524c1c8
A330454 <jacobo9cch@gmail.com>
2023-02-17 16:06:07 -0600

    cambios produccion

--- emprendedor_back/settings.py
+++ emprendedor_back/settings.py
@@ -13,11 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
-import environ
-
 # Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -35,7 +31,7 @@ ALLOWED_HOSTS = ['localhost','sistema-emprendedor-back.herokuapp.com']
 
 CORS_ALLOW_ALL_ORIGINS  = True
 
-# Application definition
+# Application definition s
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
